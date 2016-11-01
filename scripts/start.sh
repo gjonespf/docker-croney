01fdaf4aff3c9c52bcfaf4ace9388fa9bac0b7b4
gjones.pf@gmail.com <gjones.pf@gmail.com>
2016-11-02 12:39:57 +1300

    Add some verbosity

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-
-logger "storagemgr started"
+message="Croney started"
+echo $message
+logger $message
 
 chmod +x /app/scripts/init/*.sh
 #Perms sync issues for docker esp aufs
 sync; sleep 1
 
+message="Running scripts"
+echo $message
+logger $message
 for SCRIPT in /app/scripts/init/*
 	do
 		if [ -f $SCRIPT -a -x $SCRIPT ]
 		then
-         echo "Running script $SCRIPT"
+			message="Running script $SCRIPT"
+			echo $message
+			logger $message
 			$SCRIPT $@
 		fi
 	done
